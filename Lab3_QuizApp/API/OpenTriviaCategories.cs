c37f69c83446bd81be047abe585c88d459dbf209
Whezzan <d.skonneberg@gmail.com>
2025-11-12 13:18:35 +0100

    Added API folder containing OpenTrivia Categories, Questions and Responses.

---
+++ Lab3_QuizApp/API/OpenTriviaCategories.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Lab3_QuizApp.API;
+    public class OpenTriviaCategories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
---
+++ Lab3_QuizApp/API/OpenTriviaResponses.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Lab3_QuizApp.API;
+
+public class OpenTriviaResponse
+{
+    public int? response_code { get; set; }
+    public OpenTriviaQuestion[] results { get; set; }
+    public ObservableCollection<OpenTriviaCategories> trivia_categories { get; set; }
+
+    public static readonly Dictionary<int, string> ResponseMessages = new Dictionary<int, string>
+    {
+        { 0, "Questions loaded successfully." },
+        { 1, "No results for the selected parameters." },
+        { 2, "Invalid parameter values." },
+        { 3, "Token not found or expired." },
+        { 4, "Token empty; request reset." }
+    };
+    public OpenTriviaResponse()
+    {
+        trivia_categories = new ObservableCollection<OpenTriviaCategories>();
+    }
+    public static string GetResponseMessage(int? responseCode)
+    {
+        return responseCode.HasValue && ResponseMessages.ContainsKey(responseCode.Value)
+            ? ResponseMessages[responseCode.Value]
+            : "Unknown response code.";
+    }
+}
